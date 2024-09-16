e34e07579f098d4b8799a31bc82d5323b5a5b722
Clément Roger <clementro@theodo.fr>
2024-09-16 15:53:34 +0200

    books controller

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { BooksService } from 'src/books/books.service';
+import { CreateBookDTO } from 'src/books/dto/create-book.dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private booksService: BooksService) {}
+
+  @Get()
+  async getBooks() {
+    const books = await this.booksService.getBooks();
+    return books;
+  }
+
+  @Get(':bookId')
+  async getBook(@Param('bookId') bookId) {
+    const book = await this.booksService.getBook(bookId);
+    return book;
+  }
+
+  @Post()
+  async addBook(@Body() createBookDTO: CreateBookDTO) {
+    const book = await this.booksService.addBook(createBookDTO);
+    return book;
+  }
+
+  @Delete()
+  async deleteBook(@Query('bookId') bookId) {
+    const books = await this.booksService.deleteBook(bookId);
+    return books;
+  }
+}
