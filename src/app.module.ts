c9335c87f1a107a5e60bea8295d5736afec84bf9
Clément Roger <clementro@theodo.fr>
2024-09-17 11:55:04 +0200

    feat: define password in env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,25 @@ import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from 'src/books/entity/book.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     BooksModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
       port: 5432,
       username: 'postgres',
-      password: 'mysecretpassword',
+      password: process.env.POSTGRES_PASSWORD,
       database: 'postgres',
       entities: [Book],
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([Book])
+    TypeOrmModule.forFeature([Book]),
   ],
   controllers: [AppController],
   providers: [AppService],
